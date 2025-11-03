55af3238808757e7640216faf76ca8316a6feac0
2emeagauche <pychassaigne@gmail.com>
2025-11-03 15:55:00 +0100

    change typo theme strategy: using custom propoerties instead. Start overall mobile styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { useTimer } from './useTimer'
 import './assets/styles/sass/main.scss'
 
 function App() {
-  const isUnitMinute = true
+  const isUnitMinute = false
   const [taskDuration, setTaskDuration] = useState(5 * (isUnitMinute ? 60 : 1))
   const [taskDisplay, setTaskDisplay] = useState(taskDuration)
   const [shortBreakDuration, setShortBreakDuration] = useState(3 * (isUnitMinute ? 60 : 1))
   const [shortBreakDisplay, setShortBreakDisplay] = useState(shortBreakDuration)
   const [longBreakDuration, setLongBreakDuration] = useState(5 * (isUnitMinute ? 60 : 1))
   const [longBreakDisplay, setLongBreakDisplay] = useState(longBreakDuration)
-  const [fontCategory, setFontCategory] = useState('A')
-
 
   const { isPaused, start, handleStart, handleIsPaused, activeTimer } = useTimer(
     {
@@ -54,7 +52,6 @@ function App() {
     const task_value = formData.get("task_value") * (isUnitMinute ? 60 : 1)
     const short_break_value = formData.get("short_break_value") * (isUnitMinute ? 60 : 1)
     const long_break_value = formData.get("long_break_value") * (isUnitMinute ? 60 : 1)
-    const font_category = formData.get("font_category")
 
     if(task_value !== taskDuration) {
       setTaskDuration(task_value)
@@ -68,40 +65,43 @@ function App() {
       setLongBreakDuration(long_break_value)
       setLongBreakDisplay(long_break_value)
     }
-    if(font_category !== fontCategory) {
-      setFontCategory(font_category)
-    }
   }
 
   return (
     <>
-      <h1>Pomodoro</h1>
-      <p className={`text-preset-3-${fontCategory}`}><span style={{color:activeTimer===1?'red':'black'}}>Pomodoro</span></p>
-      <p className={`text-preset-3-${fontCategory}`}><span style={{color:activeTimer===2?'red':'black'}}>Short break</span></p>
-      <p className={`text-preset-3-${fontCategory}`}><span style={{color:activeTimer===3?'red':'black'}}>Long break</span></p>
-      <div className='clock'>
-        <div className='clock__hand' style={{background:`conic-gradient(#48abe0 ${progression()}%, transparent ${progression()}%) no-repeat`}}>
+      <div className='container'>
+        <h1>Pomodoro</h1>
+        <ul className='timer-steps'>
+          <li className={`timer-steps__step ${(activeTimer===1) && 'timer-steps__step--active'}`}>Pomodoro</li>
+          <li className={`timer-steps__step ${(activeTimer===2) && 'timer-steps__step--active'}`}>Short break</li>
+          <li className={`timer-steps__step ${(activeTimer===3) && 'timer-steps__step--active'}`}>Long break</li>
+        </ul>
+        <div className='clock'>
+          <div className='clock__hand' style={{background:`conic-gradient(#48abe0 ${progression()}%, transparent ${progression()}%) no-repeat`}}>
+          </div>
+          <p className='clock__numeric'>{clockDisplay(whichClock())}</p>
         </div>
-        <p className={`text-preset-1-${fontCategory} clock__numeric`}>{clockDisplay(whichClock())}</p>
+        <p><button className='button-timer' onClick={handleStart}>{start ? 'stop' : 'start'} timer</button></p>
+        <p><button className='button-timer' onClick={handleIsPaused}>{isPaused && start ? 'resume' : 'pause'} timer</button></p>
       </div>
-      <p><button className={`text-preset-2-${fontCategory}`} onClick={handleStart}>{start ? 'stop' : 'start'} timer</button></p>
-      <p><button className={`text-preset-2-${fontCategory}`} onClick={handleIsPaused}>{isPaused && start ? 'resume' : 'pause'} timer</button></p>
-      <h2 className='text-settings-preset-1-A'>Setting</h2>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend className='text-settings-preset-3-A'>Time ({isUnitMinute ? "minutes" : "seconds"})</legend>
-          <p><label className='text-settings-preset-4-A' htmlFor="task_value">pomodoro</label>: <input className='text-settings-preset-3-A' type="number" min={1} max={59} id="task_value" name="task_value" defaultValue={taskDuration / (isUnitMinute ? 60 : 1)} /></p>
-          <p><label className='text-settings-preset-4-A' htmlFor="short_break_value">short break</label>: <input className='text-settings-preset-3-A' type="number" min={1} max={59} id="short_break_value" name="short_break_value" defaultValue={shortBreakDuration / (isUnitMinute ? 60 : 1)} /></p>
-          <p><label className='text-settings-preset-4-A' htmlFor="long_break_value">long break</label>: <input className='text-settings-preset-3-A' type="number" min={1} max={59} id="long_break_value" name="long_break_value" defaultValue={longBreakDuration / (isUnitMinute ? 60 : 1)} /></p>
-        </fieldset>
-        <fieldset>
-          <legend className='text-settings-preset-3-A'>Font</legend>
-          <p><label className='text-settings-preset-2-A' htmlFor="font_A">Aa</label>: <input type="radio" value="A" defaultChecked name="font_category" id="font_A" /></p>
-          <p><label className='text-settings-preset-2-B' htmlFor="font_B">Aa</label>: <input type="radio" value="B" name="font_category" id="font_B" /></p>
-          <p><label className='text-settings-preset-2-C' htmlFor="font_C">Aa</label>: <input type="radio" value="C" name="font_category" id="font_C" /></p>
-        </fieldset>
-        <button className={`text-settings-preset-2-${fontCategory}`} type="submit">Apply</button>
-      </form>
+      <div className='dialog-setting'>
+        <h2 className='dialog-setting__title'>Setting</h2>
+        <form className='form-setting' onSubmit={handleSubmit}>
+          <fieldset>
+            <legend className='form-setting__legend'>Time ({isUnitMinute ? "minutes" : "seconds"})</legend>
+            <p><label className='form-setting__time-label' htmlFor='task_value'>pomodoro</label>: <input className='form-setting__time-input' type='number' min={1} max={59} id='task_value' name='task_value' defaultValue={taskDuration / (isUnitMinute ? 60 : 1)} /></p>
+            <p><label className='form-setting__time-label' htmlFor='short_break_value'>short break</label>: <input className='form-setting__time-input' type='number' min={1} max={59} id='short_break_value' name='short_break_value' defaultValue={shortBreakDuration / (isUnitMinute ? 60 : 1)} /></p>
+            <p><label className='form-setting__time-label' htmlFor='long_break_value'>long break</label>: <input className='form-setting__time-input' type='number' min={1} max={59} id='long_break_value' name='long_break_value' defaultValue={longBreakDuration / (isUnitMinute ? 60 : 1)} /></p>
+          </fieldset>
+          <fieldset>
+            <legend className='form-setting__legend'>Font</legend>
+            <p><label className='form-setting__font-label form-setting__font-label--A' htmlFor='font_A'>Aa</label>: <input type='radio' value='A' defaultChecked name='font_category' id='font_A' /></p>
+            <p><label className='form-setting__font-label form-setting__font-label--B' htmlFor='font_B'>Aa</label>: <input type='radio' value='B' name='font_category' id='font_B' /></p>
+            <p><label className='form-setting__font-label form-setting__font-label--C' htmlFor='font_C'>Aa</label>: <input type='radio' value='C' name='font_category' id='font_C' /></p>
+          </fieldset>
+          <button className='form-setting__submit' type='submit'>Apply</button>
+        </form>
+      </div>
     </>
   )
 }
